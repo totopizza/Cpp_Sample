beca0b035c17ac8f5ab78c62872b668ca539c8b3
tom10987 <tom10987@gmail.com>
2015-06-19 19:48:24 +0900

    不要ファイルの削除、それに伴う全体的な修正

--- 02_container_Console/GameObject.hpp
+++ 02_container_Console/GameObject.hpp
@@ -1,6 +1,7 @@
 
 #pragma once
-#include "Common.hpp"
+#include <iostream>
+#include <memory>
 #include <list>
 
 
--- 02_container_Console/Main.cpp
+++ 02_container_Console/Main.cpp
@@ -1,5 +1,5 @@
 
-#include "GameObject.h"
+#include "GameObject.hpp"
 
 
 int main() {
